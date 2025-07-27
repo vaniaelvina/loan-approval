bdcf272e36ccf89568bfab6c5228670e98b2e0a1
Vania Elvina Afiati <vaniaelvinaa@gmail.com>
2025-07-27 13:40:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import pickle
 
+# Load the trained Logistic Regression model
 with open('Logistic_Regression_model.pkl', 'rb') as file:
     Logistic_Regression_Model = pickle.load(file)
 
+# HTML Templates
 html_temp = """<div style="background-color:#000;padding:10px;border-radius:10px">
                 <h1 style="color:#fff;text-align:center">Loan Eligibility Prediction App</h1> 
                 <h4 style="color:#fff;text-align:center">Made for: Credit Team</h4> 
-                """
+             </div>"""
 
 desc_temp = """ ### Loan Prediction App 
-                This app is used by Credit team for deciding Loan Application
-                
-                #### Data Source
-                Kaggle: Link <Masukkan Link>
-                """
+This app is used by the Credit team for deciding Loan Application
+
+#### Data Source
+Kaggle: Link <Masukkan Link>
+"""
 
 def main():
     stc.html(html_temp)
@@ -31,19 +33,56 @@ def main():
 def run_ml_app():
     design = """<div style="padding:15px;">
                     <h1 style="color:#fff">Loan Eligibility Prediction</h1>
-                </div
-             """
+                </div>"""
     st.markdown(design, unsafe_allow_html=True)
-    
 
-    #If button is clilcked
-    pass
-
-def predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income
-                         ,loan_amount, loan_amount_term, credit_history, property_area):
+    # Structure Form
+    left, right = st.columns((2,2))
     
-    #Making prediction
-    pass
+    gender = left.selectbox("Gender", ("Male", "Female"))
+    married = right.selectbox("Married", ("Yes", "No"))
+    dependent = left.selectbox("Dependent", (0, 1, 2, 3))
+    education = right.selectbox("Education", ("Graduate", "Not Graduate"))
+    self_employed = left.selectbox("Self-Employed", ("Yes", "No"))
+    applicant_income = right.number_input("Applicant Income")
+    coApplicant_income = left.number_input("CoApplicant Income")
+    loan_amount = right.number_input("Loan Term")
+    loan_amount_term = left.number_input(label="Loan Amount Term", min_value=30, max_value=360)
+    credit_history = right.selectbox("Credit History", (0.0, 1.0))
+    property_area = st.selectbox("Property Area", ("Rural", "Semiurban", "Urban"))
+
+    button = st.button("Predict")
+
+    # If button is clicked
+    if button:
+        result = predict(gender, married, dependent, education, self_employed, 
+                         applicant_income, coApplicant_income, loan_amount, 
+                         loan_amount_term, credit_history, property_area)
+        
+        if result == 'Eligible':
+            st.success(f"You are {result} for the loan")
+        else:
+            st.error(f"You are {result} for the loan")
+
+def predict(gender, married, dependent, education, self_employed, 
+            applicant_income, coApplicant_income, loan_amount, 
+            loan_amount_term, credit_history, property_area):
+
+    # Preprocessing User Input
+    gen = 0 if gender == "Male" else 1
+    mar = 0 if married == "Yes" else 1
+    edu = 0 if education == "Graduate" else 1
+    sem = 0 if self_employed == "Yes" else 1
+    pro = 0 if property_area == "Semiurban" else 1 if property_area == "Urban" else 2
+
+    # Making prediction
+    prediction = Logistic_Regression_Model.predict([[
+        gen, mar, dependent, edu, sem, applicant_income, coApplicant_income,
+        loan_amount, loan_amount_term, credit_history, pro
+    ]])
+
+    result = "Not Eligible" if prediction == 0 else "Eligible"
+    return result
 
 if __name__ == "__main__":
     main()
